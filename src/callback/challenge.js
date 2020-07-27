09921f9b51f96168b1b47da2e36fc601ed71b049
Gabriel  S <gabrielmarioseno@gmail.com>
2020-07-27 12:34:31 -0300

    final de callback

--- src/callback/challenge.js
+++ src/callback/challenge.js
@@ -1,13 +1,13 @@
 let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-const API = "https://rickandmortyapi.com/api/character/";
+let API = "https://rickandmortyapi.com/api/character/";
 
 function fetchData(url_api, callback) {
-  let xhhtp = new XMLHttpRequest();
+  let xhttp = new XMLHttpRequest();
   xhttp.open("GET", url_api, true);
   xhttp.onreadystatechange = function (event) {
-    if (xhhtp.readyState === 4) {
+    if (xhttp.readyState === 4) {
       if (xhttp.status === 200) {
-        calllback(null, JSON.parse(xhttp.responseText));
+        callback(null, JSON.parse(xhttp.responseText));
       } else {
         const error = new Error("Error " + url_api);
         return callback(error, null);
